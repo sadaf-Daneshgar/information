f3f8e054bdc6bc5e816aafc190d7d32500e02169
Sadaf-Daneshgar <sadaf.barekzai54321@gmail.com>
2024-07-13 00:04:23 +0430

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const images = [
+    './image/karbala-(3).jpg',
+    './image/abas.webp',
+    './image/haram.jpg',
+    './image/اصغر.jpg',
+    './image/اصغر2.jpg',
+    './image/امام زمان.webp',
+    './image/nody-عکس-از-بین-الحرمین-در-روز-1662749110.jpg'
+]
+
+let currentImageIndex = 0;
+
+function changeImage (){
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.querySelector('#image img').src = images[currentImageIndex];
+}
+
+setInterval(changeImage, 2000);
